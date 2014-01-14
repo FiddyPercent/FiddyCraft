0780401f3ec43a8d24667eb6441a8fe14251bc98
FiddyPercent <berry.ashly@yahoo.com>
2014-01-14 02:32:37 -0700

    Added plugin.yml and Enabled and disabled code

--- src/io/github/FiddyPercent/fiddycraft/FiddyCraft.java
+++ src/io/github/FiddyPercent/fiddycraft/FiddyCraft.java
@@ -3,5 +3,16 @@ package io.github.FiddyPercent.fiddycraft;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FiddyCraft extends JavaPlugin {
-
+	
+    @Override
+    public void onEnable(){
+        this.getLogger().info("FiddyCraft is Enabled");
+    }
+ 
+    @Override
+    public void onDisable() {
+        this.getLogger().info("FiddyCraft is Disabled");
+    }
 }
+
+
