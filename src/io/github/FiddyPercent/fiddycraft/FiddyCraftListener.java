5399f64598f36d12ab30fe7d2ad16467efab18ff
FiddyPercent <berry.ashly@yahoo.com>
2014-01-16 03:40:13 -0700

    Added Start of Evidence plugin made it so the Name of the killer time and nearby players are listed in a book on death. Known issue is when non players kills it causes an issue

--- src/io/github/FiddyPercent/fiddycraft/FiddyCraftListener.java
+++ src/io/github/FiddyPercent/fiddycraft/FiddyCraftListener.java
@@ -1,14 +1,27 @@
 package io.github.FiddyPercent.fiddycraft;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 
 
 
@@ -22,7 +35,7 @@ HashMap<String, Integer> Attacked = new HashMap<String, Integer>();
 				== EntityType.PLAYER){
 			Player player = (Player) event.getEntity();
 			String name = player.getName();
-			if(!Attacked.containsKey(name) || Attacked.get(name) == 0){
+		if(!Attacked.containsKey(name) || Attacked.get(name) == 0){
 				Attacked.put(name, 1);
 				player.sendMessage(ChatColor.RED + "Run fool!");
 	  }else if(Attacked.get(name) <= 3 ){
@@ -32,11 +45,53 @@ HashMap<String, Integer> Attacked = new HashMap<String, Integer>();
 	  }else{
 		  	Attacked.put(name, 0);
 		  	player.sendMessage(ChatColor.YELLOW + "DODGE!!");
-				
 			}
-		}
-		
-		
+		}	
     }
+	
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void EntityDeath(EntityDeathEvent event){
+		String entityName = event.getEntityType().getName();
+		
+		Bukkit.broadcastMessage(ChatColor.RED + entityName + " died!");
+		
+		if(entityName.equalsIgnoreCase("Villager")){
+			ArrayList<String> witness = new ArrayList<String>();
+			Location loc = event.getEntity().getLocation();
+			List<Entity> witnesses = event.getEntity().getNearbyEntities(10, 10, 10);
+			String killer = event.getEntity().getKiller().getName();
+			String causeOfDeath = event.getEntity().getLastDamageCause().getCause().name();
+			World world = Bukkit.getWorld("world");
+			ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+			BookMeta bm = (BookMeta) book.getItemMeta();
+			String timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm").format(Calendar.getInstance().getTime());
+			
+			for(Entity y : witnesses){
+				if(y instanceof HumanEntity){
+					String name = ((HumanEntity) y).getName();
+					witness.add(name);
+				}
+			}
+				
+				
+			if(witness.isEmpty()){
+				witness.add("none");
+			}
+			bm.setAuthor("?????");
+			bm.setTitle("The Death of a Villager");
+			bm.addPage(">_>");
+			bm.setPage(1, ChatColor.RED+ "Killer: " + ChatColor.BLACK + killer + "\n" +"\n"
+					   + ChatColor.RED+ "Cause of death: "+ChatColor.BLACK + causeOfDeath + 
+					   "\n" +"\n"+ ChatColor.RED+"Time of Death: " +ChatColor.BLACK +timeStamp
+					   +"\n" + "\n" +ChatColor.GREEN+"People near by: " +ChatColor.BLACK + witness.toString());
+			book.getItemMeta().setDisplayName(ChatColor.RED+ "Evidence");
+			book.setItemMeta(bm);
+			world.dropItem(loc, book);
+			witness.clear();
+			
+		}
+	}
 	
 }
